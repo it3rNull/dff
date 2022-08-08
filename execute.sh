ea8bb490d41bda32008a7ede72bdf5b065a5c2c0
kkm213 <kkm213@korea.ac.kr>
2022-08-08 13:31:09 +0900

    upload

--- execute.sh
+++ execute.sh
@@ -2,5 +2,5 @@ git pull
 ghdl -a dff.vhd
 ghdl -a dff_tb.vhd
 ghdl -e dff_tb
-ghdl -r dff_tb --stop-time=190ns --wave=dff_tb.ghw
+ghdl -r dff_tb --stop-time=2000ns --wave=dff_tb.ghw
 gtkwave dff_tb.ghw
